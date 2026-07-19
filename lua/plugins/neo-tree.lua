27e61898a902bf0cf912cc18ddb51fa9c7758744
“krushna” <kenycalvin336699@gmail.com>
2026-07-19 14:34:08 +0530

    final update for neotree ui

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,51 +2,72 @@ return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
+		lazy = false,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
 			"nvim-tree/nvim-web-devicons",
 		},
-		lazy = false,
 
 		keys = {
-			{ "<leader>F", "<cmd>Neotree toggle<cr>", desc = "Neo-tree toggle" },
+			{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Neo-tree toggle" },
 		},
 
-		-- 2. Pass opts as an argument into the config function
-		config = function(_, opts)
-			-- Apply colors first
-			vim.api.nvim_set_hl(0, "NeoTreeFileName", { fg = "#75715E" })
-			vim.api.nvim_set_hl(0, "NeoTreeRootName", { fg = "#1E1F1C", bold = true })
-
-			git_status = {
-				window = {
-					position = "float",
-					mappings = {
-						["S"]  = "git_add_all",
-						["gu"] = "git_unstage_file",
-						["ga"] = "git_add_file",
-						["gr"] = "git_revert_file",
-						["gc"] = "git_commit",
-					}
-				}
-			}
-
-			-- Manually initialize the plugin with your opts from below
-			require("neo-tree").setup(opts)
+		init = function()
+			vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#94907D" })
 		end,
 
 		opts = {
+			enable_git_status = true,
+			highlight_git_status = true,
+
+			window = {
+				width = 30,
+				position = "left",
+				mappings = {
+					["S"]  = "git_add_all",
+					["gu"] = "git_unstage_file",
+					["ga"] = "git_add_file",
+					["gr"] = "git_revert_file",
+					["gc"] = "git_commit",
+				},
+			},
+
 			filesystem = {
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
 				filtered_items = {
 					visible = true,
-					show_hidden_count = true,
 					hide_dotfiles = false,
 					hide_gitignored = true,
-					hide_by_name = {},
-					never_show = {},
 				},
-			}
-		}
-	}
+			},
+
+			default_component_mixins = { git_status = true },
+			default_column_definitions = {
+				git_status = {
+					symbols = {
+						added     = "✚",
+						modified  = "",
+						deleted   = "✖",
+						renamed   = "  ",
+						untracked = "",
+						ignored   = "",
+						unstaged  = "  ",
+						staged    = "",
+						conflict  = "",
+
+						-- ...
+					},
+				},
+			},
+		},
+
+		config = function(_, opts)
+			vim.api.nvim_set_hl(0, "NeoTreeFileName", { fg = "#94907D" })
+			vim.api.nvim_set_hl(0, "NeoTreeRootName", { fg = "#75715E", bold = true })
+			vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#4E907D" })
+			require("neo-tree").setup(opts) -- ✅ one setup call, no return
+		end,
+	},
 }
